f0250dd7330bc7aa47b8dc69e50dc901fecaeaa8
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2022-12-05 14:23:25 +0600

    Добавил useState

--- src/components/table.js
+++ src/components/table.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Table() {
     const arr = [
@@ -6,16 +6,23 @@ function Table() {
         [21, 87, 98, 51, 87, 34],
         [12, 54, 76, 34, 34, 87],
         [21, 11, 22, 21, 98, 15]]
-    const classTable = "border-solid border border-indigo-600 p-2"
+    const [table, setTable] = useState(arr);
+    const handleChange = (e) => {
+        table[0][0] = e.target.value
+        const newTable = [...table]
+        setTable(newTable)
+    }
+
     return (
         <div>
-            {arr.map(item => {
+            {table.map((item1,index1) => {
                 return <div>
-                    {item.map(itm => <input
-                        value={itm}
-                        onClick={() => console.log('12e')}
-                        type={"text"}
-                        className={classTable + " hover:border-b-gray-900 w-10"}
+                    {item1.map((item2, index2) => <input
+                        key={[index1, index2]}
+                        value={item2}
+                        onChange={handleChange}
+                        type="text"
+                        className="border-solid border border-indigo-600 p-2 hover:border-b-gray-900 w-10"
                     />)}
                 </div>
             })}
